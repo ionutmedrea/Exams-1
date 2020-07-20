ce98548d152e52791e3baac2c06b241402b1e867
Prapugicu Alex <alexprapugicu@gmail.com>
2020-07-20 19:58:43 +0300

    Added example examActions

---
+++ client/src/actions/examActions.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+import {
+    FETCH_EXAMS,
+    EXAMS_FETCH_FAILURE
+} from "./types";
+
+function getExamsUrl() {
+    return "URL_TO_GET_API_ENDPOINT";
+}
+
+function examsFetchFailure(error) {
+    return {
+        type: EXAMS_FETCH_FAILURE,
+        payload: error
+    }
+}
+
+export const fetchExams = () => async dispatch => {
+  try{
+      const response = await axios.get(getExamsUrl());
+      return await dispatch({
+          type: FETCH_EXAMS,
+          payload: response
+      });
+  }  
+  catch (error) {
+      dispatch(examsFetchFailure(error));
+  }
+};
