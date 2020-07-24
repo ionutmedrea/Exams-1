bfac6a3b174aec63315220f5a83544aaac102501
stratidennis <des.thicklayers@gmail.com>
2020-07-24 15:46:06 +0300

    login component v 1.1

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -7,22 +7,22 @@ export class Login extends React.Component {
 
     render() {
         return (
-            <div className="base-container" ref={this.props.containerRef}>
-                <div className="header">Login</div>
-                <div className="content">\
-                    <div className="form">
-                        <div className="form-group">
-                            <label htmlFor="username">Username</label>
+            <div class="base-container" ref={this.props.containerRef}>
+                <div class="header">Login</div>
+                <div class="content">\
+                    <div class="form">
+                        <div class="form-group">
+                            <label for="username">Username</label>
                             <input type="text" name="username" placeholder="username" />
                         </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
+                        <div class="form-group">
+                            <label for="password">Password</label>
                             <input type="password" name="password" placeholder="password" />
                         </div>
                     </div>
                 </div>
-                <div className="footer">
-                    <button type="button" className="btn">
+                <div class="footer">
+                    <button type="button" class="btn">
                         Login
           </button>
                 </div>
