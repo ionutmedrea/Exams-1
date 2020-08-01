8c09461c18d2e2be452d451abad74c03ac9bf5a8
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-01 10:30:40 +0300

    updated Exam model

--- src/main/java/team/nine/Exams/models/Exam.java
+++ src/main/java/team/nine/Exams/models/Exam.java
@@ -1,100 +1,36 @@
 package team.nine.Exams.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "exams_table")
 public class Exam {
-    private @Id @GeneratedValue Long id;
-    private int academicYear;
+
+    @Id
+    @GeneratedValue
+    @Column(name= "exams_id")
+    private Long id;
+
+    @Column(name= "session")
     private String session;
+    @Column(name= "yearOfStudy")
     private int yearOfStudy;
+    @Column(name= "faculty")
     private String faculty;
+    @Column(name= "domain")
     private String domain;
+    @Column(name= "course")
     private String course;
+    @Column(name= "teacher")
     private String teacher;
+    @Column(name= "numberOfSeats")
     private int numberOfSeats;
 
-    public Exam(int academicYear, String session, int yearOfStudy, String faculty, String domain, String course, String teacher, int numberOfSeats) {
-        this.academicYear = academicYear;
-        this.session = session;
-        this.yearOfStudy = yearOfStudy;
-        this.faculty = faculty;
-        this.domain = domain;
-        this.course = course;
-        this.teacher = teacher;
-        this.numberOfSeats = numberOfSeats;
-    }
-
-    public int getAcademicYear() {
-        return academicYear;
-    }
-
-    public void setAcademicYear(int academicYear) {
-        this.academicYear = academicYear;
-    }
-
-    public String getSession() {
-        return session;
-    }
-
-    public void setSession(String session) {
-        this.session = session;
-    }
-
-    public int getYearOfStudy() {
-        return yearOfStudy;
-    }
-
-    public void setYearOfStudy(int yearOfStudy) {
-        this.yearOfStudy = yearOfStudy;
-    }
-
-    public String getFaculty() {
-        return faculty;
-    }
-
-    public void setFaculty(String faculty) {
-        this.faculty = faculty;
-    }
-
-    public String getDomain() {
-        return domain;
-    }
-
-    public void setDomain(String domain) {
-        this.domain = domain;
-    }
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
-
-    public String getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(String teacher) {
-        this.teacher = teacher;
-    }
-
-    public int getNumberOfSeats() {
-        return numberOfSeats;
-    }
-
-    public void setNumberOfSeats(int numberOfSeats) {
-        this.numberOfSeats = numberOfSeats;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
