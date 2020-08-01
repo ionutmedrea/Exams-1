69f08c5e41c955dbd10843f0cc9d9b62f53b2ac5
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-01 11:22:29 +0300

    renamed console.log print message

--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -5,7 +5,7 @@ import {userTypes} from "../actions/userTypes";
 export default function (state = initialState.exams, action) {
     switch (action.type) {
         case userTypes.FETCH_USERS:
-            console.log("FETCH_EXAMS TEST: ",action.payload);
+            console.log("FETCH_USERS TEST: ",action.payload);
             return {
                 ...state,
                 users: action.payload
