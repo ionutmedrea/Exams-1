a28faad3f8a0cd55cbe24de630bd3e3f4a87471c
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-03 18:22:48 +0300

    updated the class based component to a functional one
    Also used hooks to store the info from the form and to dispatch the registerUser request

--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,36 +1,73 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {RegisterWrapper} from "./Auth.styled";
+import {registerUser} from "../actions/userActions";
+import {useDispatch} from "react-redux";
 
-export class Register extends React.Component {
-    constructor(props) {
-        super(props);
+
+function Register() {
+
+    const dispatch = useDispatch();
+
+    const [user, setUser] = useState({
+        email: "",
+        password: "",
+        userName: "",
+    });
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (user.userName && user.email && user.password) {
+            dispatch(registerUser(user));
+        }
     }
 
-    render() {
-        return (
-            <div class="base-container" ref={this.props.containerRef}>
-                <div class="header">Register</div>
-                <div class="content">
-                    <div class="form">
-                        <div class="form-group">
-                            <label for="username">Username</label>
-                            <input type="text" name="username" placeholder="username" />
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" placeholder="email" />
-                        </div>
-                        <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="text" name="password" placeholder="password" />
-                        </div>
-                    </div>
-                </div>
-                <div class="footer">
-                    <button type="button" class="btn">
-                        Register
-          </button>
-                </div>
-            </div>
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setUser(user => ({
+                ...user,
+                [name]: value
+            })
         );
     }
+
+    return (
+        <RegisterWrapper>
+            <div>
+                <p>Registration</p>
+            </div>
+            <div>
+                <p>Let's get you started</p>
+            </div>
+            <div>
+                <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column"}}>
+                    <label>Username</label>
+                    <input type="text"
+                           placeholder="Username"
+                           name="userName"
+                           style={{textAlign: "center"}}
+                           onChange={handleChange}/>
+                    <label>Password</label>
+                    <input type="text"
+                           placeholder="Password"
+                           name="password"
+                           style={{textAlign: "center"}}
+                           onChange={handleChange}/>
+                    <label>Email</label>
+                    <input type="text"
+                           placeholder="Email"
+                           name="email"
+                           style={{textAlign: "center"}}
+                           onChange={handleChange}/>
+                    <hr/>
+                    <button onClick={() => console.log("I Registered and sent: ", user)} type="submit">Register</button>
+                </form>
+            </div>
+            <div>
+                <p>Already have an account? : Sign In</p>
+            </div>
+        </RegisterWrapper>
+    );
 }
+
+export default Register;
