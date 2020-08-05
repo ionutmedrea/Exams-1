c1848e322c6a508b012fdd12f76550bd12ddb3c2
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-05 09:39:04 +0300

    removed unused imports

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {RegisterWrapper, Wrapper} from "./Auth.styled";
+import React, {useState} from "react";
+import {Wrapper} from "./Auth.styled";
 import {registerUser} from "../actions/userActions";
 import {useDispatch} from "react-redux";
 import {Link} from "react-router-dom";
