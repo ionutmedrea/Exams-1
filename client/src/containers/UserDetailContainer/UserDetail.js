2629caac0493ec7cad0399b903315edab5b496df
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-06 13:18:21 +0300

    updated UserDetail Container to support deletion

--- client/src/containers/UserDetailContainer/UserDetail.js
+++ client/src/containers/UserDetailContainer/UserDetail.js
@@ -1,14 +1,15 @@
 import React, {useEffect} from "react";
-import {useParams} from "react-router-dom"
+import {useParams,useHistory} from "react-router-dom"
 import UserDetailCard from "../../components/users/UserDetailCard";
 import {useDispatch, useSelector} from "react-redux";
-import {startEditing, fetchUser, stopEditing} from "../../actions/userActions";
+import {startEditing, fetchUser, stopEditing, deleteUser} from "../../actions/userActions";
 import Spinner from "../../shared/Spinner";
 import UserDetailEdit from "../../components/users/UserDetailEdit";
 
 
 function UserDetail() {
 
+    const history = useHistory();
     const {username} = useParams();
 
     const user = useSelector(state => state.users.user);
@@ -16,34 +17,41 @@ function UserDetail() {
     const editMode = useSelector(state => state.users.editMode);
     const dispatch = useDispatch();
 
-    useEffect(() =>{
+    useEffect(() => {
         dispatch(fetchUser(username));
-    },[]);
+    }, []);
 
 
     function startEditingHandler() {
         dispatch(startEditing());
     }
+
     function stopEditingHandler() {
         dispatch(stopEditing());
     }
 
-return(
-    <div>
-        {
-            !editMode ?
-                user ? <UserDetailCard username={username}
-                                            user={user}
-                                            editMode={editMode}
-                                            startEdit={startEditingHandler}/> : <Spinner/>
-            : user ? <UserDetailEdit username={username}
-                          user={user}
-                          editMode={editMode}
-                          stopEdit={stopEditingHandler}/> : <Spinner/>
-        }
+    function deleteUserHandler() {
+        dispatch(deleteUser(user));
+        history.goBack();
+    }
 
-    </div>
-);
+    return (
+        <div>
+            {
+                !editMode ?
+                    user ? <UserDetailCard username={username}
+                                           user={user}
+                                           editMode={editMode}
+                                           startEdit={startEditingHandler}/> : <Spinner/>
+                    : user ? <UserDetailEdit username={username}
+                                             user={user}
+                                             editMode={editMode}
+                                             stopEdit={stopEditingHandler}
+                                             deleteUser={deleteUserHandler}/> : <Spinner/>
+            }
+
+        </div>
+    );
 }
 
 export default UserDetail;
