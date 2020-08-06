9adffe4ad27902effe525307a34aff1ae03bfe34
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-06 18:57:49 +0300

    added filter registration bean

--- src/main/java/team/nine/Exams/ExamsApplication.java
+++ src/main/java/team/nine/Exams/ExamsApplication.java
@@ -2,13 +2,25 @@ package team.nine.Exams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import team.nine.Exams.filter.CORSFilter;
 
 @SpringBootApplication
-
 public class ExamsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExamsApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilterRegistration(){
+		FilterRegistrationBean registrationBean =
+				new FilterRegistrationBean(new CORSFilter());
+		registrationBean.setName("CORS FILTER");
+		registrationBean.addUrlPatterns("/*");
+		registrationBean.setOrder(1);
+		return registrationBean;
 	}
 
 }
