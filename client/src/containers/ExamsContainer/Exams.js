66ba3bf9b10bee950acc065e588af97d22ce33d4
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-06 13:17:26 +0300

    updated Exams container to display more info
    Also added a button for adding new exams

--- client/src/containers/ExamsContainer/Exams.js
+++ client/src/containers/ExamsContainer/Exams.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchExams} from "../../actions/examActions";
+import ExamCard from "../../components/exams/ExamCard";
+import Spinner from "../../shared/Spinner";
+import {ExamsWrapper, Wrapper} from "./exams.styled";
+import {Link, useHistory} from "react-router-dom";
 
 
 function Exams() {
-    return(
-        <div>
-            <p>Exams Container</p>
-        </div>
+    const exams = useSelector(state => state.exams.exams);
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+
+
+    useEffect(() => {
+        dispatch(fetchExams());
+    }, []);
+
+    return (
+        <Wrapper>
+            <p>Search</p>
+            <p>Filter</p>
+            <Link to={"/add"}>
+                <button>Add exam</button>
+            </Link>
+            <ExamsWrapper>
+                {exams ? exams.map(exam => <ExamCard key={exam.id}
+                                                     id={exam.id}
+                                                     faculty={exam.faculty}
+                                                     yearOfStudy={exam.yearOfStudy}
+                                                     teacher={exam.teacher}
+                                                     course={exam.course}/>)
+                    : <Spinner/>}
+            </ExamsWrapper>
+        </Wrapper>
     );
 }
 
