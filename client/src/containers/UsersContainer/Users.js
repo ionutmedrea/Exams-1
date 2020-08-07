ffbf18c1b56ea49b9fe3370285f9cb62630aae8c
Prapugicu Alex <alexprapugicu@gmail.com>
2020-08-07 21:18:42 +0300

    updated users container

--- client/src/containers/UsersContainer/Users.js
+++ client/src/containers/UsersContainer/Users.js
@@ -3,7 +3,14 @@ import {useDispatch, useSelector} from "react-redux";
 import Spinner from "../../shared/Spinner";
 import {fetchUsers} from "../../actions/userActions";
 import UserCard from "../../components/users/UserCard";
-import {SearchBarWrapper, UserContainer, UserListStyled} from "./UserList.styled";
+import {
+    FancyFilterWrapper,
+    FancyLabel, FancySelect,
+    FancySelectWrapper,
+    SearchBarWrapper,
+    UserContainer,
+    UserListStyled
+} from "./UserList.styled";
 import UsersSearchForm from "./UsersSearchForm";
 
 function Exams() {
@@ -18,19 +25,32 @@ function Exams() {
 
     return (
         <UserContainer>
-            <SearchBarWrapper>
-                <UsersSearchForm/>
-                <p>filter by</p>
-            </SearchBarWrapper>
-        <UserListStyled>
-            {users ? users.map(user => <UserCard
-                key={user.uid}
-                id={user.uid}
-                username={user.userName}
-                password={user.password}
-                email={user.email}
-                role={user.role}/>) : <Spinner/>}
-        </UserListStyled>
+            <div style={{margin: "5%"}}>
+                <SearchBarWrapper>
+                    <UsersSearchForm/>
+                    <FancySelectWrapper>
+                        <FancyFilterWrapper>
+                            <FancyLabel htmlFor="filters">Filter</FancyLabel>
+                        <FancySelect name="filters" id="filters" defaultValue="">
+                            <option disabled defaultValue=""></option>
+                            <option value="byUserName">Filter by Username Ascending</option>
+                            <option value="byUserName">Filter by Username Descending</option>
+                            <option value="byEmail">Filter by Email Ascending</option>
+                            <option value="byEmail">Filter by Email Descending</option>
+                        </FancySelect>
+                        </FancyFilterWrapper>
+                    </FancySelectWrapper>
+                </SearchBarWrapper>
+                <UserListStyled>
+                    {users ? users.map(user => <UserCard
+                        key={user.uid}
+                        id={user.uid}
+                        username={user.userName}
+                        password={user.password}
+                        email={user.email}
+                        role={user.role}/>) : <Spinner/>}
+                </UserListStyled>
+            </div>
         </UserContainer>
     );
 }
